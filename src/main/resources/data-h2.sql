03db99b9fded03da3a07c8b7b3438a52fb57c797
Olivier <Olivier@192.168.1.10>
2018-05-26 15:50:35 +0200

    Ajout de donné à l'initialisation

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,4 @@
+INSERT INTO USER (id, email, password) VALUES
+  (HIBERNATE_SEQUENCE.nextval, 'admin', 'password'),
+  (HIBERNATE_SEQUENCE.nextval, 'm.constans.olivier@gmail.com', 'password'),
+  (HIBERNATE_SEQUENCE.nextval, 'jojo@gmail.com', 'password');
